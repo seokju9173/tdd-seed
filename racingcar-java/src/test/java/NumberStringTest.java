d1ae2079d07f617241bab7ed9e1b6ee6484cda8e
김우재 <kwj1270@naver.com>
2022-01-18 22:47:46 +0900

    test: 정수1과2를 ,로 분리했을 때 정상 동작한다

---
+++ racingcar-java/src/test/java/NumberStringTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("숫자 문자열")
+class NumberStringTest {
+}
---
+++ racingcar-java/src/test/java/NumberStringsTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NumberStringsTest {
+
+    // 실패 테스트 케이스
+    @Test
+    void 정수_1과_2를_콤마로_분리했을_때_잘_분리된다() {
+        final NumberStrings numberStrings = new NumberStrings("1,2");
+
+        final List<String> numberStrings2 = numberStrings.split(",");
+
+        assertThat(numberStrings2).containsExactly("1", "2");
+    }
+
+}
