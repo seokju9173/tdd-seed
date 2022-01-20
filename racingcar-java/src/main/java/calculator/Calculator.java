280f5e9dcac2b06d5a7f69789702fc9815cf5535
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-20 17:13:04 +0900

    refactor: 피드백에 맞게 코드 수정

--- racingcar-java/src/main/java/calculator/Calculator.java
+++ racingcar-java/src/main/java/calculator/Calculator.java
@@ -1,76 +1,79 @@
 package calculator;
 
-import java.util.regex.Pattern;
+import java.util.Objects;
 
 public class Calculator {
 
-    private static final String[] operation = {"+", "-", "*", "/"};
-    private static final Pattern STRING_NUMBER = Pattern.compile("^[0-9]*$");
+    private static final String PLUS = "+";
+    private static final String MINUS = "-";
+    private static final String DIVIDE = "/";
+    private static final String MULTI = "*";
+    private static final String BLANK = " ";
 
     int calculator(String input) {
-        isNull(input);
+        checkIsNull(input);
 
-        String[] splitStr = input.split(" ");
+        String[] splitStr = input.split(BLANK);
+        checkIsEmpty(splitStr);
 
-        int answer = 0;
-        String currentOperation = "+";
+        int answer = parseInt(splitStr[0]);
 
-        for (int i = 0; i < splitStr.length; i++) {
-            // 공백인지 모든 경우 검사
-            isEmpty(splitStr[i]);
-
-            // 숫자일 경우
-            if (i % 2 == 0) {
-                switch (currentOperation) {
-                    case "+":
-                        answer += isNumber(splitStr[i]);
-                        break;
-                    case "-":
-                        answer -= isNumber(splitStr[i]);
-                        break;
-                    case "*":
-                        answer *= isNumber(splitStr[i]);
-                        break;
-                    case "/":
-                        answer /= isNumber(splitStr[i]);
-                        break;
-                }
-            }
-
-            // 사칙연산일 경우
-            else {
-                currentOperation = isOperation(splitStr[i]);
-            }
+        for (int i = 1; i < splitStr.length - 1; i += 2) {
+            answer = calculate(answer, splitStr[i], parseInt(splitStr[i + 1]));
         }
         return answer;
     }
 
-    private String isOperation(String oper) {
-        for (String str : operation) {
-            if (str.equals(oper)) {
-                return oper;
-            }
-        }
+    private int calculate(int firstValue, String operator, int secondValue) {
+        if (operator.equals(PLUS))
+            return add(firstValue, secondValue);
+        if (operator.equals(MINUS))
+            return subtract(firstValue, secondValue);
+        if (operator.equals(MULTI))
+            return multiply(firstValue, secondValue);
+        if (operator.equals(DIVIDE))
+            return divide(firstValue, secondValue);
         throw new IllegalArgumentException("사칙연산이 아님");
     }
 
-    private void isEmpty(String str) {
+    private int add(int i, int j) {
+        return i + j;
+    }
+
+    private int subtract(int i, int j) {
+        return i - j;
+    }
+
+    private int multiply(int i, int j) {
+        return i * j;
+    }
+
+    private int divide(int i, int j) {
+        // 나눗셈의 결과는 문제에서 정수로 떨어지는 결과에 한정하여
+        // 굳이 divide by zero 처리는 하지 않음
+        return i / j;
+    }
+
+    private void checkIsEmpty(String[] str) {
         // 인풋 자체에 "  "가 있을경우 공백이 포함될 경우도 있다.
-        if (str.isEmpty()) {
-            throw new IllegalArgumentException("공백 포함");
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].isEmpty()) {
+                throw new IllegalArgumentException("공백 포함");
+            }
         }
     }
 
-    private void isNull(String str) {
-        if (str == null) {
+    private void checkIsNull(String str) {
+        if (Objects.isNull(str)) {
             throw new IllegalArgumentException("입력값이 null 임");
         }
     }
 
-    private int isNumber(String str) {
-        if (!STRING_NUMBER.matcher(str).matches()) {
+    private int parseInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자 아닌 값 포함");
         }
-        return Integer.parseInt(str);
     }
 }
--- racingcar-java/src/test/java/calculator/CalculatorTest.java
+++ racingcar-java/src/test/java/calculator/CalculatorTest.java
@@ -3,8 +3,6 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.regex.Pattern;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
@@ -13,68 +11,72 @@ class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
-    @DisplayName("덧셈")
-    void 문자열계산기_덧셈() {
+    @DisplayName("문자열계산기 덧셈")
+    void calculatorPlusTest() {
         assertThat(calculator.calculator("2 + 3")).isEqualTo(5);
     }
 
     @Test
-    @DisplayName("뺄셈")
-    void 문자열계산기_뺄셈() {
+    @DisplayName("문자열계산기 뺄셈")
+    void calculatorMinusTest() {
         assertThat(calculator.calculator("3 - 1")).isEqualTo(2);
     }
 
     @Test
-    @DisplayName("곱셈")
-    void 문자열계산기_곱셈() {
+    @DisplayName("문자열계산기 곱셈")
+    void calculatorMultiTest() {
         assertThat(calculator.calculator("3 * 2")).isEqualTo(6);
     }
 
     @Test
-    @DisplayName("나눗셈")
-    void 문자열계산기_나눗셈() {
+    @DisplayName("문자열계산기 나눗셈")
+    void calculatorDivideTest() {
         assertThat(calculator.calculator("9 / 3")).isEqualTo(3);
     }
 
     @Test
-    @DisplayName("null")
-    void 문자열계산기_null() {
+    @DisplayName("문자열계산기 null 테스트")
+    void calculatorNullTest() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() ->
-                        calculator.calculator(null)
+                .isThrownBy(() -> calculator.calculator(null)
                 ).withMessageMatching("입력값이 null 임");
     }
 
     @Test
-    @DisplayName("숫자 아님")
-    void 문자열계산기_숫자아님() {
+    @DisplayName("문자열계산기 숫자 아님")
+    void calculatorNotNumber() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() ->
-                        calculator.calculator("ㅋㅋ")
+                .isThrownBy(() -> calculator.calculator("ㅋㅋ")
                 ).withMessageMatching("숫자 아닌 값 포함");
     }
 
     @Test
-    @DisplayName("사칙연산 아님")
-    void 문자열계산기_사칙연산아님() {
+    @DisplayName("문자열계산기 사칙연산 아님")
+    void calculatorNotOperation() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() ->
-                        calculator.calculator("1 ㅋㅋ")
+                .isThrownBy(() -> calculator.calculator("1 ㅋㅋ 1")
                 ).withMessageMatching("사칙연산이 아님");
     }
 
     @Test
-    @DisplayName("공백 포함")
-    void 문자열계산기_공백포함() {
+    @DisplayName("문자열계산기 공백 포함")
+    void calculatorIncludeBlank() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() ->
-                        calculator.calculator("1  + 3")
+                .isThrownBy(() -> calculator.calculator("")
                 ).withMessageMatching("공백 포함");
     }
 
     @Test
-    @DisplayName("사칙연산 모두 포함")
-    void 문자열계산기_사칙연산산() {
+    @DisplayName("문자열계산기 식에 공백 포함")
+    void calculatorIncludeBlank2() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> calculator.calculator("1 +  3")
+                ).withMessageMatching("공백 포함");
+    }
+    
+    @Test
+    @DisplayName("문자열계산기 사칙연산 모두 포함")
+    void calculatorAllOperation() {
         assertThat(calculator.calculator("2 + 3 * 4 / 2 - 5")).isEqualTo(5);
     }
 }
