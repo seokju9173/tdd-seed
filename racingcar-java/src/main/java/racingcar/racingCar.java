d6bc74a2177eaf497ac0449d3c97178e880fed84
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-21 10:32:40 +0900

    feat: racingCar.java 생성

---
+++ racingcar-java/src/main/java/racingcar/racingCar.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+public class racingCar {
+    String progress = "";
+
+    public racingCar(String progress) {
+        this.progress = progress;
+    }
+
+    public String getProgress() {
+        return progress;
+    }
+
+    public void setProgress(String progress) {
+        this.progress = progress;
+    }
+}
