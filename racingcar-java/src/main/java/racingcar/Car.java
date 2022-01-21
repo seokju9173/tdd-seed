a41a67710539346282682ccb3b45db55dca58cc2
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-21 11:11:26 +0900

    refactor: Car.java에 매직 리터럴 적용

--- racingcar-java/src/main/java/racingcar/Car.java
+++ racingcar-java/src/main/java/racingcar/Car.java
@@ -3,12 +3,15 @@ package racingcar;
 import java.util.Random;
 
 public class Car {
+    private static final int MAX_BOUND = 10;
+    private static final String PROGRESS_STRING = "-";
+
     private static Random random = new Random();
     private String progress = "";
 
     void drivingCar () {
-        if (random.nextInt(10) > 3){
-            this.progress += "-";
+        if (random.nextInt(MAX_BOUND) > 3){
+            this.progress += PROGRESS_STRING;
         }
     }
 
