57399b00cb6f09ae8835abdc054cfa02259b1032
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-22 02:09:23 +0900

    refactor: Car.java 매직 넘버 추가 설정

--- racingcar-java/src/main/java/racingcar/Car.java
+++ racingcar-java/src/main/java/racingcar/Car.java
@@ -5,12 +5,13 @@ import java.util.Random;
 public class Car {
     private static final int MAX_BOUND = 10;
     private static final String PROGRESS_STRING = "-";
+    private static final int ALLOW_DRIVE = 4;
 
-    private static Random random = new Random();
+    private static final Random random = new Random();
     private String progress = "";
 
     void drivingCar () {
-        if (random.nextInt(MAX_BOUND) > 3){
+        if (random.nextInt(MAX_BOUND) >= ALLOW_DRIVE){
             this.progress += PROGRESS_STRING;
         }
     }
