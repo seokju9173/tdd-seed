df0c9cda22aa5b673eb2f1026d54125e20f4fd2f
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-22 12:42:03 +0900

    refactor: OutputView.java 에서 현재 차 위치 출력하도록 개선

--- racingcar-java/src/main/java/racingcar/view/OutputView.java
+++ racingcar-java/src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,12 @@
 package racingcar.view;
 
+import racingcar.Car;
+
 public class OutputView {
     private static final String PRINT_HOW_MANY_CARS = "자동차 대수는 몇 대 인가요?";
     private static final String PRINT_HOW_MANY_PROGRESS = "시도할 회수는 몇 회 인가요?";
     private static final String PRINT_RESULT = "\n실행 결과";
+    private static final String PROGRESS_STRING = "-";
 
     public static void printHowManyCars(){
         System.out.println(PRINT_HOW_MANY_CARS);
@@ -15,5 +18,18 @@ public class OutputView {
 
     public static void printResult(){
         System.out.println(PRINT_RESULT);
+    }
+
+    public static void printCurrentCarsPosition(Car[] cars){
+        StringBuilder sb = new StringBuilder();
+
+        for (Car car : cars){
+            for (int i = 0; i< car.getProgress(); i++){
+                sb.append(PROGRESS_STRING);
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
