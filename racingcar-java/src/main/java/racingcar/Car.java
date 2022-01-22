b917404befab4b4d00919ebd9a7526b41bc52fb3
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-22 12:43:15 +0900

    refactor: Car.java 의 progress 타입변경, 그에 따라 Race.java 에서 받아오는 형식도 변경

--- racingcar-java/src/main/java/racingcar/Car.java
+++ racingcar-java/src/main/java/racingcar/Car.java
@@ -4,19 +4,20 @@ import java.util.Random;
 
 public class Car {
     private static final int MAX_BOUND = 10;
-    private static final String PROGRESS_STRING = "-";
     private static final int ALLOW_DRIVE = 4;
+    private static final int START_LINE = 0;
+    private static final int DRIVE_CAR_AT_ONE_CYCLE = 1;
 
     private static final Random random = new Random();
-    private String progress = "";
+    private int progress = START_LINE;
 
     void drivingCar () {
         if (random.nextInt(MAX_BOUND) >= ALLOW_DRIVE){
-            this.progress += PROGRESS_STRING;
+            this.progress += DRIVE_CAR_AT_ONE_CYCLE;
         }
     }
 
-    public String getProgress() {
+    public int getProgress() {
         return progress;
     }
 }
--- racingcar-java/src/main/java/racingcar/Race.java
+++ racingcar-java/src/main/java/racingcar/Race.java
@@ -11,30 +11,39 @@ public class Race {
     public void racePrepare() {
         OutputView.printHowManyCars();
         this.carsTotal = InputView.inputCarCount();
+
         OutputView.printHowManyProgress();
         this.progressTotal = InputView.inputProgressTotal();
+
         setupCars();
-        race();
+
+        raceResult();
     }
 
-    public void race() {
-        OutputView.printResult();
-
-        //아래 for문을 추가적으로 매소드를 분리 시킬수 있을 것 같음
-        for (int i = 0; i < progressTotal; i++) {
-            // 아래 구문을 OutputView로 이동해야함
-            for (int j = 0; j < carsTotal; j++) {
-                cars[j].drivingCar();
-                System.out.println(cars[j].getProgress());
-            }
-            System.out.println();
-        }
-    }
-
-    public void setupCars(){
+    private void setupCars() {
         cars = new Car[carsTotal];
         for (int i = 0; i < carsTotal; i++) {
             cars[i] = new Car();
+        }
+    }
+
+    private void raceResult() {
+        OutputView.printResult();
+
+        for (int i = 0; i < progressTotal; i++) {
+            raceOneCycle();
+        }
+    }
+
+    private void raceOneCycle() {
+        moveCars(cars);
+        OutputView.printCurrentCarsPosition(cars);
+    }
+
+    //한 사이클에 모든 차들 전진 혹은 정차
+    private void moveCars(Car[] cars) {
+        for (int j = 0; j < carsTotal; j++) {
+            cars[j].drivingCar();
         }
     }
 }
