929b240d6e58517d124e415613045afb901c8e35
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-23 12:21:54 +0900

    feat: Car.java 에 carName 추가

--- racingcar-java/src/main/java/racingcar/Car/Car.java
+++ racingcar-java/src/main/java/racingcar/Car/Car.java
@@ -5,7 +5,12 @@ public class Car {
     private static final int START_LINE = 0;
     private static final int DRIVE_CAR_AT_ONE_CYCLE = 1;
 
+    private final String carName;
     private int position = START_LINE;
+
+    public Car(String carName) {
+        this.carName = carName;
+    }
 
     public void drivingCar(int commandNumber) {
         if (commandNumber >= ALLOW_DRIVE) {
@@ -15,5 +20,9 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    public String getCarName() {
+        return carName;
     }
 }
