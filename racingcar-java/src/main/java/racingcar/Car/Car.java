6f0ca991009687c695a3969fcc88caaf211df86f
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-23 23:47:01 +0900

    refactor: Car 의 이름 길이 매직 넘버 처리

--- racingcar-java/src/main/java/racingcar/Car/Car.java
+++ racingcar-java/src/main/java/racingcar/Car/Car.java
@@ -4,12 +4,13 @@ public class Car {
     private static final int ALLOW_DRIVE = 4;
     private static final int START_LINE = 0;
     private static final int DRIVE_CAR_AT_ONE_CYCLE = 1;
+    private static final int LIMITED_NAME_LENGTH = 5;
 
     private final String carName;
     private int position = START_LINE;
 
     public Car(String carName) {
-        if (carName.length() > 5){
+        if (carName.length() > LIMITED_NAME_LENGTH){
             throw new RuntimeException("차량의 이름은 5자를 넘길 수 없습니다.");
         }
         this.carName = carName;
