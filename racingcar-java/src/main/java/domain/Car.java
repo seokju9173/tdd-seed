9c521dcf497381796a4daa451ee3d648b5987686
yeonju501 <loling970124@gmail.com>
2022-01-26 01:16:26 +0900

    refactor: 매직넘버 처리, static -> 일반 변수 순서 변경

--- racingcar-java/src/main/java/domain/Car.java
+++ racingcar-java/src/main/java/domain/Car.java
@@ -2,19 +2,22 @@ package domain;
 
 public class Car {
 
-    private String name;
-
-    private int position = DEFAULT_POSITION;
-
     private static final int CONDITION_MOVE = 4;
 
     private static final int DEFAULT_POSITION = 0;
 
     private static final int MAX_NAME_LENGTH = 5;
 
+    private static final int MIN_RANDOM = 0;
+
+    private static final int MAX_RANDOM = 10;
+
+    private final String name;
+
+    public int position = DEFAULT_POSITION;
+
     public Car (String name) {
         validateCarName(name);
-
         this.name = name;
     }
 
@@ -26,14 +29,13 @@ public class Car {
 
     public void move(int randomInt) {
         validateInputRange(randomInt);
-
         if (randomInt >= CONDITION_MOVE) {
             position++;
         }
     }
 
     private void validateInputRange(int randomInt) {
-        if (randomInt < 0 || randomInt >= 10) {
+        if (randomInt < MIN_RANDOM || randomInt >= MAX_RANDOM) {
             throw new IllegalArgumentException("0에서 9사이의 숫자여야 합니다");
         }
     }
