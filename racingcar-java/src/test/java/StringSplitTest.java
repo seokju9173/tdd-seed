77dafa4db94d2db2a7830e3129c70ec71208aad6
nGyu <vpdls1511@gmail.com>
2022-04-29 21:41:05 +0900

    feat(vpdls1511-step1): substring() 기능 이용한 출력 구현

--- racingcar-java/src/test/java/StringSplitTest.java
+++ racingcar-java/src/test/java/StringSplitTest.java
@@ -5,12 +5,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringSplitTest {
     @Test
-    void splitTest_1(){
+    void splitTest(){
         String[] str1 = "1,2".split(",");
         String[] str2 = "1".split(",");
 
         assertThat(str1).contains("1","2");
         assertThat(str2).contains("1");
+    }
 
+    @Test
+    void subStringTest(){
+        String str = "(1,2)".substring(1,4);
+
+        assertThat(str).isEqualTo("1,2");
     }
 }
