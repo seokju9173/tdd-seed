6f6e25b879dd37e4540b23708930918f1bd7448c
jaehoo1 <jaehoo8936@naver.com>
2022-05-01 02:31:25 +0900

    test(jaehoo1):SetTest, StringTest 클래스 리팩터링

--- racingcar-java/src/test/java/SetTest.java
+++ racingcar-java/src/test/java/SetTest.java
@@ -1,16 +1,15 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
 import java.util.HashSet;
 import java.util.Set;
-
 import static org.assertj.core.api.Assertions.*;
 
-final class SetTest {
+class SetTest {
     private Set<Integer> numbers;
 
     @BeforeEach
@@ -24,19 +23,27 @@ final class SetTest {
 
     // Test Case 구현
     @Test
-    public final void setSiseTest() {
-        assertThat(numbers.size()).isEqualTo(3);
+    @DisplayName("집합 크기가 3인지 확인")
+    void setSizeTest() {
+        assertThat(numbers).hasSize(3);
     }
 
-    @ParameterizedTest  //dependencies: 'org.junit.jupiter:junit-jupiter-params:5.4.2'
+    @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    public final void containsTest(int number) {
+    @DisplayName("1, 2, 3 값이 존재하는지 확인")
+    void containsTest(int number) {
         assertThat(numbers.contains(number)).isTrue();
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
-    public final void containsVarietyTest(String input, String expected) {
-        Assertions.assertEquals(Boolean.toString(numbers.contains(Integer.parseInt(input))), expected);
+    @DisplayName("존재/존재하지 않는 값 테스트")
+    void containsVarietyTest(String input, String expected) {
+        Assertions.assertEquals(
+                Boolean.toString(
+                        numbers.contains(
+                                Integer.parseInt(input)
+                        )
+                ), expected);
     }
 }
--- racingcar-java/src/test/java/StringTest.java
+++ racingcar-java/src/test/java/StringTest.java
@@ -1,24 +1,28 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.*;
 
-final class StringTest {
+class StringTest {
 
     @Test
-    public final void splitTest() {
-        assertThat("1,2".split(",")).contains("1", "2");
-        assertThat("1".split(",")).containsExactly("1");
+    @DisplayName("문자열을 ,로 분리")
+    void splitTest() {
+        Assertions.assertAll(
+                () -> assertThat("1,2".split(",")).contains("1", "2"),
+                () -> assertThat("1".split(",")).containsExactly("1")
+        );
     }
 
     @Test
-    public final void bracketRemoveTest() {
+    @DisplayName("부분문자열 추출 테스트")
+    void bracketRemoveTest() {
         assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
     }
 
     @Test
-    @DisplayName("charAt() 위치 값을 벗어나는 부분 확인")
-    public final void charAtExceptionTest() {
+    @DisplayName("문자열 위치 값을 벗어나는 부분 확인")
+    void charAtExceptionTest() {
         assertThatThrownBy(() -> "abc".charAt(3))
                 .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
