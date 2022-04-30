c7baa6de8b963125e7ac1b6e49e618c3914b81e8
nGyu <vpdls1511@gmail.com>
2022-04-30 22:46:48 +0900

    test(vpdls1511): 테스트 이름 Alias 추가 및 수정

--- racingcar-java/src/test/java/StringTest.java
+++ racingcar-java/src/test/java/StringTest.java
@@ -5,9 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-
+@DisplayName("문자열 학습 테스트")
 public class StringTest {
+
     @Test
+    @DisplayName("split() 학습 테스트")
     void splitTest(){
         String[] str1 = "1,2".split(",");
         String[] str2 = "1".split(",");
@@ -17,6 +19,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("substring() 학습 테스트")
     void subStringTest(){
         String str = "(1,2)".substring(1,4);
 
@@ -24,7 +27,7 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("Exception 처리")
+    @DisplayName("Exception 발생 시 예외처리 학습 테스트")
     void charAtTest(){
         String str = "abc";
 
