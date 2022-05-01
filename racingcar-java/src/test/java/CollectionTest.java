084a07a71e71e2b49af7d23ca8a1a674a2b65320
nGyu <vpdls1511@gmail.com>
2022-05-01 12:54:18 +0900

    test(vpdls1511) : 테스트 이름과 코드 컨벤션 맞춰  수정

--- racingcar-java/src/test/java/CollectionTest.java
+++ racingcar-java/src/test/java/CollectionTest.java
@@ -5,13 +5,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-
 
 @DisplayName("Collection 학습 테스트")
 public class CollectionTest {
@@ -30,25 +28,20 @@ public class CollectionTest {
     @Test
     @DisplayName("numbers Set 사이즈 조회 학습 테스트")
     void setTest1() {
-        int collectionSize = numbers.size();
-        assertAll(
-                () -> assertThat(collectionSize).isEqualTo(3),
-                () -> assertThat(collectionSize).isNotEqualTo(4)
-        );
+        assertThat(numbers).hasSize(3);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1,2,3})
+    @ValueSource(ints = {1, 2, 3})
     @DisplayName("ParameterizedTest, ValueSource 어노테이션을 이용한 여러 케이스 테스트")
-    void setTest2(int num){
+    void setTest2(int num) {
         assertThat(numbers.contains(num)).isTrue();
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1,true","2,true","3,true","4,false","5,false"})
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
     @DisplayName("ParameterizedTest, ValueSource 어노테이션을 이용한 여러 케이스 테스트")
-    void setTest3(int num, boolean expected){
+    void setTest3(int num, boolean expected) {
         assertThat(numbers.contains(num)).isEqualTo(expected);
     }
-
 }
--- racingcar-java/src/test/java/StringTest.java
+++ racingcar-java/src/test/java/StringTest.java
@@ -10,25 +10,27 @@ public class StringTest {
 
     @Test
     @DisplayName("split() 학습 테스트")
-    void splitTest(){
+    void splitTest() {
         String[] str1 = "1,2".split(",");
         String[] str2 = "1".split(",");
 
-        assertThat(str1).contains("1","2");
-        assertThat(str2).contains("1");
+        assertAll(
+                () -> assertThat(str1).contains("1", "2"),
+                () -> assertThat(str2).contains("1")
+        );
     }
 
     @Test
     @DisplayName("substring() 학습 테스트")
-    void subStringTest(){
-        String str = "(1,2)".substring(1,4);
+    void subStringTest() {
+        String str = "(1,2)".substring(1, 4);
 
         assertThat(str).isEqualTo("1,2");
     }
 
     @Test
-    @DisplayName("Exception 발생 시 예외처리 학습 테스트")
-    void charAtTest(){
+    @DisplayName("특정 위치의 문자를 가져오는 학습 테스트")
+    void charAtTest() {
         String str = "abc";
 
         assertThatThrownBy(() -> str.charAt(3))
