77649efd312d89234a7649750a9762f708cf1406
nGyu <vpdls1511@gmail.com>
2022-05-04 20:52:56 +0900

    test(vpdls1511) : 자동차 경주 테스트

---
+++ racingcar-java/src/test/java/step3/RacingTest.java
@@ -0,0 +1,21 @@
+package step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+@DisplayName("레이싱 테스트")
+class RacingTest {
+    Racing racing = new Racing();
+
+    @Test
+    @DisplayName("입력값 넣기 테스트")
+    void testRacing(){
+        String input = "3\n5";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        racing.racingCar();
+    }
+}
