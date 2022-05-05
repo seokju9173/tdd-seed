46c1c218c2299f58778827b3d128198e217e5064
nGyu <vpdls1511@gmail.com>
2022-05-05 14:31:39 +0900

    test(vpdls1511) : 테스트코드 수정

--- racingcar-java/src/test/java/step3/RacingTest.java
+++ racingcar-java/src/test/java/step3/RacingTest.java
@@ -28,29 +28,11 @@ class RacingTest {
     @Test
     @DisplayName("Car에 값 넣고 이동하기")
     void useCarObjectTest() {
-        Cars cars = new Cars(IntStream.range(0, 3)
-                .mapToObj(Car::new)
-                .collect(Collectors.toList()));
+        Cars cars = new Cars(3);
 
         outputView.outputMoveCarPosition(cars);
         cars.getCars().get(0).move();
         outputView.outputMoveCarPosition(cars);
-    }
-
-    @Test
-    @DisplayName("경주 기능 테스트")
-    void racingLogicTest() {
-        Cars cars = new Cars(IntStream.range(0, 3)
-                .mapToObj(Car::new)
-                .collect(Collectors.toList()));
-
-        outputView.outputMoveCarPosition(cars);
-
-        for (Car car: cars.getCars()) {
-            car.move();
-            outputView.outputMoveCarPosition(cars);
-            System.out.println("");
-        }
     }
 
 }
