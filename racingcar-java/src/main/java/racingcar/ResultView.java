d7fb645b2980c7ad434b14c446930025fd7c0b5f
jaehoo1 <jaehoo8936@naver.com>
2022-05-11 22:03:31 +0900

    refactor(jaehoo1) : ResultView 클래스 Stream 리팩터링

--- racingcar-java/src/main/java/racingcar/ResultView.java
+++ racingcar-java/src/main/java/racingcar/ResultView.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ResultView {
 
@@ -14,15 +15,11 @@ public class ResultView {
         System.out.println("시도할 회수는 몇 회 인가요?");
     }
 
-    public static void printMessage() {
-        System.out.println("실행 결과");
-    }
-
     public static void printGameStatus(Cars cars) {
         List<String> carsStatus = cars.getCarsStatus();
-        for(int i = 0; i < carsStatus.size(); i++) {
-            System.out.println(carsStatus.get(i));
-        }
+        IntStream.range(0, carsStatus.size()).
+                mapToObj(i -> carsStatus.get(i)).
+                forEach(System.out::println);
         System.out.println();
     }
 
