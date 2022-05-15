9bc45f132da77af04f2d00fea0e229c305d51b38
jaehoo1 <jaehoo8936@naver.com>
2022-05-16 01:40:32 +0900

    refactor(jaehoo1) : Game 클래스 리팩터링

--- racingcar-java/src/main/java/racingcar/Game.java
+++ racingcar-java/src/main/java/racingcar/Game.java
@@ -3,6 +3,7 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalInt;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Game {
@@ -11,12 +12,12 @@ public class Game {
 
         String names = InputView.getNames();
         cars = new Cars(new MoveStrategyRandom(), names);
-        IntStream.range(0, cars.getCarList().size()).
-                forEach(i -> cars.getCar(i));
+        IntStream.range(0, cars.getCarList().size())
+                .forEach(i -> cars.getCar(i));
         int round = InputView.getRoundNumber();
         
-        IntStream.range(0, round).
-                forEach(i -> {
+        IntStream.range(0, round)
+                .forEach(i -> {
                     cars.moveCars();
                     ResultView.printGameStatus(cars);
                 });
@@ -26,16 +27,14 @@ public class Game {
     }
 
     public List<Car> getWinner(Cars cars) {
-        List<Car> winnerCars = new ArrayList<>();
-
-        OptionalInt optionalMax = IntStream.range(0, cars.getCarList().size()).
-                map(i -> cars.getCar(i).getPosition()).
-                max();
-        int maxPosition = optionalMax.getAsInt();
-        IntStream.range(0, cars.getCarList().size()).
-                filter(i -> cars.getCar(i).getPosition() == maxPosition).
-                mapToObj(cars::getCar).
-                forEach(winnerCars::add);
+        int maxPosition = IntStream.range(0, cars.getCarList().size())
+                .map(i -> cars.getCar(i).getPosition())
+                .max()
+                .getAsInt();
+        List<Car> winnerCars = IntStream.range(0, cars.getCarList().size())
+                .filter(i -> cars.getCar(i).getPosition() == maxPosition)
+                .mapToObj(cars::getCar)
+                .collect(Collectors.toList());
 
         if(winnerCars.size() == 0) {
             throw new IllegalArgumentException("승자가 0명인 오류 발생");
