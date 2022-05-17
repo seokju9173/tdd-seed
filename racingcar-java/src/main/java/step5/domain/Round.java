f7143127d5838ab888f9467887e8e35ad07cd180
nGyu <vpdls1511@gmail.com>
2022-05-17 20:33:52 +0900

    feat(vpdls1511) : Round 객체 안에 Cars 객체 추가 후 이동 가능 및 테스트

---
+++ racingcar-java/src/main/java/step5/domain/Round.java
@@ -0,0 +1,26 @@
+package step5.domain;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Round {
+    private final Map<Integer, Cars> round;
+
+    public Round(Map<Integer, Cars> round) {
+        this.round = round;
+    }
+
+    public Round(final int roundNum, final Cars cars){
+        this.round = new HashMap<>();
+        this.round.put(roundNum, cars);
+    }
+
+    public void add(final int roundNum, final Cars cars) {
+        round.put(roundNum, cars);
+    }
+
+    public Map<Integer, Cars> getRound() {
+        return Collections.unmodifiableMap(round);
+    }
+}
