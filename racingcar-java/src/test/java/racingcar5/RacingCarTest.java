c245a00456e2d442b3df874f543706a2e250b69a
jaehoo1 <jaehoo8936@naver.com>
2022-05-21 00:27:00 +0900

    test(jaehoo1) : Position 클래스 테스트 코드 작성

--- racingcar-java/src/test/java/racingcar5/RacingCarTest.java
+++ racingcar-java/src/test/java/racingcar5/RacingCarTest.java
@@ -9,6 +9,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RacingCarTest {
 
     @Test
+    @DisplayName("위치 객체 테스트")
+    void positionCreateTest() {
+        Position position1 = new Position();
+        Position position2 = new Position(2);
+        Position position3 = new Position(3);
+        position3.increase();
+        assertAll(
+                () -> assertThat(position1.getDistance()).isEqualTo(1),
+                () -> assertThat(position2.getDistance()).isEqualTo(2),
+                () -> assertThat(position3.getDistance()).isEqualTo(4)
+        );
+    }
+
+    @Test
     @DisplayName("자동차 생성 테스트")
     void carCreateTest() {
         Car car = new Car("pobi");
