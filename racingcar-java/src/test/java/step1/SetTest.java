dbc66fc8e09939f0daa6b51a6ea75ead2aaeb3e7
seokju9173 <85390517+seokju9173@users.noreply.github.com>
2022-10-18 23:10:58 +0900

    test(seokju9173):SetTest클래스 contains 기능 결과값 true, false 반환 테스트

--- racingcar-java/src/test/java/step1/SetTest.java
+++ racingcar-java/src/test/java/step1/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -35,5 +36,12 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void containsTest(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("contains 실행 결과 true, false 반환")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsTrueFalseTest(int number, boolean result) {
+        assertThat(numbers.contains(number)).isEqualTo(result);
     }
 }
