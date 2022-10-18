2d5ca51c5123fa6872d5f78caad3c4d57141cb31
seokju9173 <85390517+seokju9173@users.noreply.github.com>
2022-10-18 23:09:40 +0900

    test(seokju9173):SetTest클래스 contains 기능 중복 제거 테스트

--- racingcar-java/src/test/java/step1/SetTest.java
+++ racingcar-java/src/test/java/step1/SetTest.java
@@ -3,6 +3,8 @@ package step1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -20,11 +22,18 @@ public class SetTest {
         numbers.add(2);
         numbers.add(3);
     }
-    
+
     @Test
     @DisplayName("Set 크기 확인")
     void sizeTest() {
         int setSize = numbers.size();
         assertThat(setSize).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("1,2,3의 값이 존재하는지 확인하는 코드의 중복 코드 제거")
+    @ValueSource(ints = {1, 2, 3})
+    void containsTest(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
