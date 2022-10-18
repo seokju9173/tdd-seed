6ab8985a3a1911f038706652d286c9ddcc604bdf
seokju9173 <85390517+seokju9173@users.noreply.github.com>
2022-10-18 23:03:35 +0900

    test(seokju9173):StringTest클래스 charAt 기능 테스트

---
+++ racingcar-java/src/test/java/step1/StringTest.java
@@ -0,0 +1,36 @@
+package step1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+    @Test
+    @DisplayName("문자열 분리")
+    void splitTest() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+    }
+
+    @Test
+    @DisplayName("문자열 분리, 원소가 정확히 일치")
+    void splitExactlyTest() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    @DisplayName("문자열 자르기")
+    void substringTest() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자 가져오기")
+    void charAtTest() {
+        String string = "abc";
+        char result = string.charAt(0);
+        assertThat(result).isEqualTo('a');
+    }
+}
