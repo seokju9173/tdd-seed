702cb1e6e7af55858912546133344c4b84758e44
seokju9173 <85390517+seokju9173@users.noreply.github.com>
2022-10-18 19:11:10 +0900

    test(seokju9173):StringTest클래스 split 기능 테스트

---
+++ racingcar-java/src/test/java/study/StringTest.java
@@ -0,0 +1,18 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    void split(){
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+    }
+
+    @Test
+    void splitExactly(){
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1","2");
+    }
+}
